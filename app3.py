9f780132bae7fb18d1ae2dac4d0c4947b000f25b
fgmg10 <163671815+fgmg10@users.noreply.github.com>
2024-06-16 01:14:12 -0400

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+import pip
+pip.main(["install", "openpyxl"])
+
+st.title('Aprende a subir tu base de datos de Excel a la Web')
+df = pd. read_excel ('USD-COP.xisx')
+
+st.write(df)
